2e674ca532b8b77c09023d28f7281bf8ccf7fa1c
yordisev <yordys12@gmail.com>
2021-09-17 15:02:55 -0500

    rutas vue js

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+</div>        
+    <!-- jQuery, Popper.js, Bootstrap JS -->
+    <script src="jquery/jquery-3.3.1.min.js"></script>
+    <script src="popper/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>         
+    <!--Vue.JS -->    
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>              
+    <!--Axios -->      
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.15.2/axios.js"></script>    
+    <!--Sweet Alert 2 -->        
+    <script src="plugins/sweetalert2/sweetalert2.all.min.js"></script>      
+    <!--Código custom -->          
+    <script src="main.js"></script>         
+    </body>
+</html>
---
+++ includes/navbar.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+    <head>
+    <link rel="shortcut icon" href="#" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- Bootstrap CSS -->    
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <!-- FontAwesom CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">        
+    <!--Sweet Alert 2 -->
+    <link rel="stylesheet" href="plugins/sweetalert2/sweetalert2.min.css">        
+    <!--CSS custom -->  
+    <link rel="stylesheet" href="main.css">  
+    </head>
+    <body>
+    <header>
+        <h2 class="text-center text-dark"><span class="badge badge-success">CRUD con VUE.JS</span></h2>
+    </header>    
+    
+     <div id="appMoviles"> 
