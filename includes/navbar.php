5f3696669c4910c525ab8714a66d816074fd0077
yordisev <yordys12@gmail.com>
2021-11-22 14:57:30 -0500

    htacces

--- includes/navbar.php
+++ includes/navbar.php
@@ -30,12 +30,12 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <a class="navbar-brand" href="prueba.php">Prueba</a>
+  <a class="navbar-brand" href="prueba">Prueba</a>
 
   <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index">Home <span class="sr-only">(current)</span></a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
